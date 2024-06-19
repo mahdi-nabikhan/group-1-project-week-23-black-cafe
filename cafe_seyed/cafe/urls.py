cc931bad388b3c42f75b1acea647ca83c4dded67
mahdi-nabikhan <156818753+mahdi-nabikhan@users.noreply.github.com>
2024-06-19 15:42:59 +0330

    view are modified

--- cafe_seyed/cafe/urls.py
+++ cafe_seyed/cafe/urls.py
@@ -6,6 +6,6 @@ from .views import *
 app_name = 'cafe'
 urlpatterns = [path('', CategoryListView.as_view(), name='landing_page'),
                path('item/<int:category_id>/', ProductListView.as_view(), name='items'),
-               path('detail/<int:item_id>/', ItemDetail.as_view(), name='detail'),
-               path('carts/', cart_detail, name='cart_detail'), ]
+               path('detail/<int:item_id>/', ItemDetail.as_view(), name='detail')]
+               # path('carts/', cart_detail, name='cart_detail')
 # path('order/',order_items,name='order'),]
--- cafe_seyed/cafe/views.py
+++ cafe_seyed/cafe/views.py
@@ -30,31 +30,32 @@ class CategoryListView(ListView):
 
 
 class ProductListView(View):
+    template_name = 'landing_page/category_items.html'
 
     def get(self, request, category_id):
         category = Categories.objects.get(id=category_id)
         products = Products.objects.filter(category=category)
         context = {'products': products, 'category': category}
-        return render(request, 'landing_page/category_items.html', context)
+        return render(request, self.template_name, context)
 
 
-def items_detail(request, item_id):
-    c = Products.objects.filter(id=item_id)
-    if request.method == 'POST':
-        form = OrderForm(request.POST)
-        if form.is_valid():
-            form1 = form.cleaned_data
-            product = Products.objects.get(id=item_id)
-            user = User.objects.get(id=form1['user_id'])
-            total = form1['quantity'] * product.price
-            OrderItem(product_id=product, user_id=user, quantity=form1['quantity'], price=product.price,
-                      discount=0, total_price=total).save()
-            Cart.objects.create(user_id=user, total_amount=total)
-            return redirect('cafe:landing_page')
-
-    else:
-        form = OrderForm()
-        return render(request, 'landing_page/details.html', {'product': c, 'form': form})
+# def items_detail(request, item_id):
+#     c = Products.objects.filter(id=item_id)
+#     if request.method == 'POST':
+#         form = OrderForm(request.POST)
+#         if form.is_valid():
+#             form1 = form.cleaned_data
+#             product = Products.objects.get(id=item_id)
+#             user = User.objects.get(id=form1['user_id'])
+#             total = form1['quantity'] * product.price
+#             OrderItem(product_id=product, user_id=user, quantity=form1['quantity'], price=product.price,
+#                       discount=0, total_price=total).save()
+#             Cart.objects.create(user_id=user, total_amount=total)
+#             return redirect('cafe:landing_page')
+#
+#     else:
+#         form = OrderForm()
+#         return render(request, 'landing_page/details.html', {'product': c, 'form': form})
 
 
 class ItemDetail(View):
@@ -94,15 +95,15 @@ class ItemDetail(View):
 #         return render(request, 'form_order.html', {'form': form})
 
 
-def cart_detail(request):
-    form = CartForm()
-    cart = Cart()
-    if request.method == 'POST':
-        form = CartForm(request.POST)
-        if form.is_valid():
-            form1 = form.cleaned_data
-            cart.objects.get(id=form1['user_id'])
-            return redirect('cafe:landing_page')
-    else:
-        context = {'form': form, 'cart': cart}
-        return render(request, 'landing_page/forms/cart_views.html', context)
+# def cart_detail(request):
+#     form = CartForm()
+#     cart = Cart()
+#     if request.method == 'POST':
+#         form = CartForm(request.POST)
+#         if form.is_valid():
+#             form1 = form.cleaned_data
+#             cart.objects.get(id=form1['user_id'])
+#             return redirect('cafe:landing_page')
+#     else:
+#         context = {'form': form, 'cart': cart}
+#         return render(request, 'landing_page/forms/cart_views.html', context)

d07c181f278354384e4516ca7c73e6db7a98e69c
navid <navid.kheyri@yahoo.com>
2024-06-24 00:20:12 +0330

    login and logout

---
+++ cafe_seyed/accounts/views.py
@@ -0,0 +1,27 @@
+from django.shortcuts import render,redirect
+from django.contrib.auth import authenticate, login, logout
+from django.http import HttpResponse
+from cafe.urls import *
+
+# Create your views here.
+
+
+def loginview(request):
+
+    if request.method == 'POST':
+
+        username=request.POST["username"]
+        password=request.POST["password"]
+        user = authenticate(request,username=username,password=password)
+        if user is not None:
+            login(request , user)
+            return redirect('cafe:landing_page')
+        else:
+            return HttpResponse('Invalid Login')
+        
+    return render (request,'accounts/login.html')
+
+
+def logoutview(request):
+    logout(request)
+    return redirect('cafe:landing_page')
--- cafe_seyed/cafe/urls.py
+++ cafe_seyed/cafe/urls.py
@@ -5,20 +5,20 @@ from .views import *
 
 app_name = 'cafe'
 urlpatterns = [path('', CategoryListView.as_view(), name='landing_page'),
-               path('item/<int:category_id>/', ProductListView.as_view(), name='items'),
-               path('detail/<int:item_id>/', ItemDetail.as_view(), name='detail'),
-               path('carts/', cart_detail, name='cart_detail'),
-               path('search/', search_products, name='search'),
-               path('about_us/', about_us, name='about_us'),
-               path('contact_us/', contact_us, name='contact_us'),
-               path('detail/<int:item_id>/', ItemDetail.as_view(), name='detail'),
+            path('item/<int:category_id>/', ProductListView.as_view(), name='items'),
+            path('detail/<int:item_id>/', ItemDetail.as_view(), name='detail'),
+            path('carts/', cart_detail, name='cart_detail'),
+            path('search/', search_products, name='search'),
+            path('about_us/', about_us, name='about_us'),
+            path('contact_us/', contact_us, name='contact_us'),
+            path('detail/<int:item_id>/', ItemDetail.as_view(), name='detail'),
 
-               path('showcart/',ShowCarts.as_view(), name='showcart'),
-               path('tickets/',Ticket.as_view(), name='ticket'),]
-              
+            path('showcart/',ShowCarts.as_view(), name='showcart'),
+            path('tickets/',Ticket.as_view(), name='ticket'),
+            
 
-               path('showcart/', ShowCarts.as_view(), name='showcart'),
-               path('tickets/', Ticket.as_view(), name='ticket'),
+            path('showcart/', ShowCarts.as_view(), name='showcart'),
+            path('tickets/', Ticket.as_view(), name='ticket'),
                ]
 
 

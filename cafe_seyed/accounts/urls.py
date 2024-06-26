7cceeb3aec6434170d65ff5767acecea70caee62
sirse7en <mh.rezvannia@gmail.com>
2024-06-26 15:04:49 +0330

    "Create Registeration view"

--- cafe_seyed/accounts/urls.py
+++ cafe_seyed/accounts/urls.py
@@ -6,5 +6,6 @@ from .views  import *
 
 app_name = 'accounts'
 urlpatterns = [path('login/',loginview,name='login'),
-               path('logout/',logoutview,name='logout')
-               ]
+               path('logout/',logoutview,name='logout'),
+path('rigerster/',RegisterView.as_view(),name='register'),
+]
--- cafe_seyed/accounts/views.py
+++ cafe_seyed/accounts/views.py
@@ -28,3 +28,16 @@ def loginview(request):
 def logoutview(request):
     logout(request)
     return redirect('cafe:landing_page')
+
+
+class RegisterView(View):
+    def post(self, request):
+        if request.POST['password'] != request.POST['confirm_password']:
+            return HttpResponse('Passwords do not match')
+
+        user = User(username=request.POST['username'], email=request.POST['email'], password=request.POST['password'])
+        user.save()
+        return redirect('cafe:landing_page')
+
+    def get(self, request):
+        return render(request, 'accounts/registeration.html')
